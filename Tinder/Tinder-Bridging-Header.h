e0e1152f95329254929c64e2d16609a52332f272
Vincent Renais <vincentrenais@gmail.com>
2015-05-26 10:02:32 -0400

    Login with Facebook

---
+++ Tinder/Tinder-Bridging-Header.h
@@ -0,0 +1,12 @@
+// Parse
+
+#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
+#import <Bolts/Bolts.h>
+
+
+// Facebook
+
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
