ca3d9400122a2ba7228cfe27a94bdc0286aa0921
Vincent Renais <vincentrenais@gmail.com>
2015-05-26 15:12:35 -0400

    Facebook login and data fetching working

---
+++ Tinder/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  Tinder
+//
+//  Created by Vincent Renais on 2015-05-26.
+//  Copyright (c) 2015 Vincent Renais. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+   
+    var firstname: String!
+    var lastname: String!
+    var username: String!
+    var latitude: Double!
+    var longitude: Double!
+    var userPicture: String!
+    
+}
