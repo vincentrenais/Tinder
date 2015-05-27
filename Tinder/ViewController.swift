54bca5d93305a3a00f6224e5db8c6d9de8cfae6c
Vincent Renais <vincentrenais@gmail.com>
2015-05-27 10:35:45 -0400

    Swipe functionality is working

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -8,9 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
     
     @IBOutlet var FacebookLoginButton: FBSDKLoginButton!
+    
+    let pageTitles = ["", "", "", ""]
+    
+    var images = ["long3.png","long4.png","long1.png","long2.png"]
+    
+    var count = 0
+    
+    var pageViewController : UIPageViewController!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +32,89 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         {
             // User is already logged in, do work such as go to next view controller.
         }
+        
+        reset()
     }
+    
+    
+    
+    @IBAction func swipeLeft(sender: AnyObject) {
+        println("Swipe left")
+    }
+    @IBAction func swiped(sender: AnyObject) {
+        
+        self.pageViewController.view.removeFromSuperview()
+        self.pageViewController.removeFromParentViewController()
+        reset()
+    }
+    
+    
+    func reset() {
+        /* Getting the page View controller */
+        pageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("PageViewController") as! UIPageViewController
+        self.pageViewController.dataSource = self
+        
+        let pageContentViewController = self.viewControllerAtIndex(0)
+        self.pageViewController.setViewControllers([pageContentViewController!], direction: UIPageViewControllerNavigationDirection.Forward, animated: true, completion: nil)
+        
+        /* We are substracting 30 because we have a start again button whose height is 30*/
+        self.pageViewController.view.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height - 30)
+        self.addChildViewController(pageViewController)
+        self.view.addSubview(pageViewController.view)
+        self.pageViewController.didMoveToParentViewController(self)
+    }
+    
+    @IBAction func start(sender: AnyObject) {
+        let pageContentViewController = self.viewControllerAtIndex(0)
+        self.pageViewController.setViewControllers([pageContentViewController!], direction: UIPageViewControllerNavigationDirection.Forward, animated: true, completion: nil)
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        var index = (viewController as! PageContentVC).pageIndex!
+        index++
+        if(index == self.images.count){
+            return nil
+        }
+        return self.viewControllerAtIndex(index)
+        
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        var index = (viewController as! PageContentVC).pageIndex!
+        if(index == 0){
+            return nil
+        }
+        index--
+        return self.viewControllerAtIndex(index)
+        
+    }
+    
+    func viewControllerAtIndex(index : Int) -> UIViewController? {
+        if((self.pageTitles.count == 0) || (index >= self.pageTitles.count)) {
+            return nil
+        }
+        let pageContentViewController = self.storyboard?.instantiateViewControllerWithIdentifier("PageContentViewController") as! PageContentVC
+        
+        pageContentViewController.imageName = self.images[index]
+        pageContentViewController.titleText = self.pageTitles[index]
+        pageContentViewController.pageIndex = index
+        return pageContentViewController
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return pageTitles.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
+    
+    
+    
+    
     
     // Facebook Delegate Methods
     
